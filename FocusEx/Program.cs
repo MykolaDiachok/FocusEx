31d57663a9c53d9e655a99d1a15d26d1a5b10018
NikeDyachok <ndyachok@gmail.com>
2016-02-13 20:35:32 +0200

    Add first commit

---
+++ FocusEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NetFwTypeLib;
+
+namespace FocusEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://www.codeproject.com/Articles/14906/Open-Windows-Firewall-During-Installation
+            http://blogs.msdn.com/b/securitytools/archive/2009/08/21/automating-windows-firewall-settings-with-c.aspx
+            Type NetFwMgrType = Type.GetTypeFromProgID("HNetCfg.FwMgr", false);
+            INetFwMgr mgr = (INetFwMgr)Activator.CreateInstance(NetFwMgrType);
+            bool Firewallenabled = mgr.LocalPolicy.CurrentProfile.FirewallEnabled;
+            Console.WriteLine("{0}", Firewallenabled);
+            Console.ReadKey();
+        }
+    }
+}
